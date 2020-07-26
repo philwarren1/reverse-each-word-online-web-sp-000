6c69b27e7626b67d3926a81635ea59696da5a525
Philip Warren <philwarren1@gmail.com>
2020-07-26 16:12:03 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,14 +3,7 @@ def reverse_each_word(sentence)
   array = sentence.split(" ")
   new_array = []
   array.each do |sentence|
-    new_array << 
-  end  
-end
-def reverse_each_word_with_each(string)
-  original_array = string.split(" ")
-  return_array = []
-  original_array.each do|string|
-    return_array << string.reverse
+    new_array << sentence.reverse
   end
-  return_array.join(" ")
+  new_array.join(" ")
 end
