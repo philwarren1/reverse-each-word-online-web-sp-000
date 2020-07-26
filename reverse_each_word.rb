36ebb8fc502fa3df94c020c1415bd436ed6563de
Philip Warren <philwarren1@gmail.com>
2020-07-26 16:00:22 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 sentence1 = []
 def reverse_each_word(sentence1)
   sentence1.each do |sentence1|
-    "#{sentence1}"
+    "#{sentence1}".reverse
   end  
 end
 toppings = ["pickles", "mushrooms", "bacon"]
